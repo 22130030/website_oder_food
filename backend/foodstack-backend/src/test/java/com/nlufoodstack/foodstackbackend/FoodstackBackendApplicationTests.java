92a9ce69f00c3a10c5ce4152721b4226e633d8f8
22130030 <22130030@st.hcmuaf.edu.vn>
2026-04-16 20:48:50 +0700

    Tạo trang thực đơn món ăn, Tạo project Spring boot cho backend, và kết nối MySql database

---
+++ backend/foodstack-backend/src/test/java/com/nlufoodstack/foodstackbackend/FoodstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlufoodstack.foodstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodstackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
